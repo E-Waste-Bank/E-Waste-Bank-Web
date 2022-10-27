1e9e58f0eebc19e2984e4362a77fa8abac1f27b1
raditya.aditama <raditya.aditama@ui.ac.id>
2022-10-27 13:54:57 +0700

    Update models

--- keuangan/models.py
+++ keuangan/models.py
@@ -4,4 +4,4 @@ from django.contrib import auth
 # Create your models here.
 class KeuanganAdmin(models.Model):
     uang_user  = models.FloatField()
-    user = models.ForeignKey(auth.User, on_delete=models.CASCADE)
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
