a68d7950491fd0eb2060ec4efea9f4d2f6141863
Hugo Sulaiman Setiawan <hugo.sulaiman@ui.ac.id>
2022-10-29 22:51:13 +0700

    Implement show keuangan and show keuangan user

--- keuangan/views.py
+++ keuangan/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
 from keuangan.models import KeuanganAdmin, Cashout
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.contrib.auth.models import User
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect,JsonResponse
+from django.urls import reverse
+from django.core import serializers
 
 # Create your views here.
+@login_required
+def show_keuangan(request: HttpRequest):
+    if request.user.groups.exists():
+        groups = request.user.groups.all()
+
+        for group in groups:
+            if group.name == "admin":
+                return HttpResponseRedirect(reverse("keuangan:show_admin"))
+
+        return HttpResponseRedirect(reverse("keuangan:show_user"))
+
 def show_admin(request):
     keuangan_admin = KeuanganAdmin.objects.all()
 
@@ -11,3 +26,13 @@ def show_admin(request):
     }
 
     return render(request, "admin.html", context)
+
+@login_required
+def show_user(request: HttpRequest): # test DONE
+    data_keuangan = KeuanganAdmin.objects.get(user = request.user)
+
+    context = {
+        'data_keuangan': data_keuangan,
+    }
+
+    return render(request, "user.html", context)
