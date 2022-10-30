2fd885c310cc75a84ce00bb2ab70a7f6e278e39e
Hugo Sulaiman Setiawan <hugo.sulaiman@ui.ac.id>
2022-10-30 21:34:04 +0700

    Update forms.py to use ModelForm

--- keuangan/forms.py
+++ keuangan/forms.py
@@ -1,5 +1,11 @@
 from .models import *
 from django import forms
 
-class CreateCashoutForm(forms.Form):
-    amount = forms.FloatField(required=True, min_value=0, widget=forms.NumberInput(attrs={"class":"form-control"}))
+class CreateCashoutForm(forms.ModelForm):
+    class Meta:
+        model = Cashout
+        fields = ['amount']
+
+    def __init__(self, *args, **kwargs):
+        super(CreateCashoutForm, self).__init__(*args, **kwargs)
+        self.fields['amount'].widget.attrs.update({'class': 'form-control'})
