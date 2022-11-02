06de9b69e05467a57e564b0956f2e06d2219b227
rionadani <rionadani@gmail.com>
2022-11-02 18:27:54 +0700

    Update 5 about_us

--- about_us/static/js/add_feedback.js
+++ about_us/static/js/add_feedback.js
@@ -3,11 +3,11 @@ function get_latest_three(){
     $.getJSON("/about-us/get-latest-three", function (data) {
         $.each(data, function(i, entry) {
             cards+=`
-            <div class="card">
-                <div class="card-body">
+            <div class="card" >
+                <div class="card-body" style="background-color:#F8F7F3">
                     <blockquote class="blockquote mb-0">
                         <p>${entry.fields.your_feedback}</p>
-                    <footer class="blockquote-footer" style="background-color:white">${entry.fields.name} on ${entry.fields.date}</footer>
+                    <footer class="blockquote-footer" style="background-color:#F8F7F3">${entry.fields.name} on ${entry.fields.date}</footer>
                     </blockquote>
                 </div>
             </div>
@@ -30,14 +30,12 @@ $(document).ready(function(){
             data: $(this).serialize(),
             success: function (data) {
                 $("#exampleModal").modal("toggle");
-                $(".modal-body > p > input").each(function (i) {
-                    $(this).val("");
-                });
                 get_latest_three()
             }, 
             error: function (jqxhr, status, error) {
             },       
         })
+        document.getElementById("form_add").reset()
     })
 })
 
