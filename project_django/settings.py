47ef30d537421f5cd19c4fb04e2435719fe449e4
Hugo Sulaiman Setiawan <hugo.sulaiman@ui.ac.id>
2022-12-02 13:10:17 +0700

    Install django-cors-headers

--- project_django/settings.py
+++ project_django/settings.py
@@ -51,9 +51,11 @@ INSTALLED_APPS = [
     'tips_and_tricks',
     'about_us',
     'authentication',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
