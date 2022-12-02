feca90b21e0ca83d3f595f02a362bdf2f071ace1
Hugo Sulaiman Setiawan <hugo.sulaiman@ui.ac.id>
2022-12-02 13:24:05 +0700

    Implement JSON login view with role checker

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,43 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login as auth_login
+from django.http import HttpRequest, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+@csrf_exempt
+def login(request: HttpRequest):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        if user.is_active:
+            auth_login(request, user)
+            
+            # asumsikan di awal user bkn admin
+            role = "user"
+
+            # check if user is admin
+            if request.user.groups.exists():
+                groups = request.user.groups.all()
+
+                for group in groups:
+                    if group.name == "admin":
+                        role = "admin"
+                        break
+            
+            return JsonResponse({
+                "status": True,
+                "message": "Successfully logged in!",
+                "role": role
+            }, status=200)
+        
+        else:
+            return JsonResponse({
+                "status": False,
+                "message": "Failed to login, account is disabled."
+            }, status=401)
+
+    else:
+        return JsonResponse({
+            "status": False,
+            "message": "Failed to login, check your email/password."
+        }, status=401)
