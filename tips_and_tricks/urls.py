dda21d52fd86611ca8522dd683edcdbfb38d063f
hayfa_najma <hayfanajmaa22@gmail.com>
2022-12-08 17:48:28 +0700

    update for mobile

--- tips_and_tricks/urls.py
+++ tips_and_tricks/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import index, add, load_more, search_json
+from .views import *
 
 urlpatterns = [
     path('', index, name='index_tips'),
     path('add', add, name='add_tips'),
     path('load-more', load_more, name='load-more'),
     path('search-json', search_json, name='search_json'),
+    path('post-json', get_all_post, name='get_all_post'),
+    path('add-mobile', add_mobile, name='add_mobile'),
+    # path('get-username/<int:idUser>', get_user_username, name='get_user_username'),
 ]
