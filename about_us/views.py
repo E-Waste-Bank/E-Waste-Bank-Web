055cf20731cb560dcb2b28c6e34cab3b2323cdd5
rionadani <rionadani@gmail.com>
2022-12-12 19:53:09 +0700

    edit view

--- about_us/views.py
+++ about_us/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .forms import *
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from .models import *
 from django.core import serializers
 from django.views.decorators.csrf import csrf_exempt
